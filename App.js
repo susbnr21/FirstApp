9a951aebc06538f5f29b424e067573a97dd46e79
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 16:05:57 +0545

    Adding an image

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
+export default class Bananas extends Component {
+  render () {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello Everyone This is Anatomy Moves AKA.'AM' and welcome to my first app. </Text>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}} style={{justifyContent="center"}}/>
     );
   }
 }
