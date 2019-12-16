c3f4d6548cff2ac883b46def71b21ccaf1891e91
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 16:42:45 +0545

    Added splash

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import Splash from './app/splash';
-import { Text, View } from 'react-native';
-
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{flex: 0.75, alignItems: "center", justifyContent: "center"}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
-  }
-}
+//import { Text, View } from 'react-native';
 
 export default class LotsOfGreeting extends Component {
   render() {
