28e0225ca886d2b4754f1e64ff3b135e409a1cf1
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 16:48:04 +0545

    Adding Hello World

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Splash from './splash';
-//import { Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class LotsOfGreeting extends Component {
   render() {
     return (
-      <Splash/>
+      <View style={{alignItems: "center"}}>
+        <Text> Hello World !!!</Text>
+      </View>
     );
   }
 }
