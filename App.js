72d444b03d24d939003b847072f2f445af3abdd5
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 16:53:00 +0545

    Adding Hello World

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native';
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+      <View style={{alignItems: "center", flex: 1}}>
+        <Text> Hello World !!! </Text>
       </View>
     );
   }
