5d5a58664fb6206fedf602ab9b8d1b2557362e39
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 15:55:08 +0545

    Adding few text message

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+        <Text>Hello Everyone This is Anatomy Moves AKA.'AM' and welcome to my first app. </Text>
       </View>
     );
   }
