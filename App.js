9049224b84d0b82c4ea62dde33fef0408d910c7b
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 16:24:34 +0545

    Adding names

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 class Greeting extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
         <Text>Hello {this.props.name}!</Text>
       </View>
     );
@@ -14,7 +14,7 @@ class Greeting extends Component {
 export default class LotsOfGreeting extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
           <Greeting name = 'Sushil'/>
           <Greeting name = 'Sulav'/>
           <Greeting name = 'Summit'/>
