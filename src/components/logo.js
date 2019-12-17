5e58ea90d0fad01d8dcaf14d649ee229eaf836a6
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-17 12:36:24 +0545

    Adding Styles

---
+++ src/components/logo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, StatusBar } from 'react-native';
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <Text></Text>
+        );
+    }
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,9 +4,18 @@ import { Text, View, StyleSheet, StatusBar } from 'react-native';
 export default class Login extends Component {
     render() {
         return (
-            <View>
-                <Text>Login</Text>
+            <View style={styles.container}>
+                
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+      backgroundColor: '#455a64',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  });
