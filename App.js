5c5d3f4ce4e129438d0ce0e965b878eabe410bee
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-17 12:21:20 +0545

    Adding Styles

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, StatusBar } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+      <StatusBar
+      backgroundColor= "white"
+      barStyle="light-content"
+      />
         <Text style={{color: '#ffffff', fontSize: 20}}>Hello There!!!</Text>
       </View>
     );
