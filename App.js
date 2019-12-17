81c0ab2a1e4c63c75391a0da7e8acf4276ae8dee
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-17 12:07:25 +0545

    Adding styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
