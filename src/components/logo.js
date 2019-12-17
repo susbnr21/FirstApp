feb6a2bba70cc860db9f160f55a70a03015d6608
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-17 12:45:52 +0545

    Adding Image

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet, StatusBar, Image } from 'react-native';
 export default class Logo extends Component {
     render() {
         return (
-            <Image style={{width: 80, height: 100}}
+            <Image style={{width: 120, height: 120}}
             source={require('../images/Logo.png')}/>
         );
     }
